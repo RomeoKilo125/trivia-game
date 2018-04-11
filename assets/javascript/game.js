27bde3823cf7bc5937da30beb9101717a1d6e66f
RomeoKilo125 <romeokilo@icloud.com>
2018-04-11 12:32:28 -0400

    functioning lifelines

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,19 +5,28 @@ game = function() {
   let minScore = 0;
   let timer = 0;
   let q = '';
+  let used5050 = false;
+  let usedJump = false;
+  let usedDouble = false;
+  let x2 = false;
+
   setupGame = function() {
     qArray = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14];
     score = 0;
     minScore = 0;
     timer = 30;
+    used5050 = false;
+    usedJump = false;
+    usedDouble = false;
     $('#questionBox').html('<img id="startButton" src="assets/images/startButton.png" />');
     $('.answerBox').text("answers go here");
-    $('#5050Box').html('<img src="assets/images/unused5050.png" />')
-    $('#jumpBox').html('<img src="assets/images/unusedjump.png" />')
-    $('#doubleBox').html('<img src="assets/images/unusedDouble.png" />')
+    // $('#5050Box').html('<img src="assets/images/unused5050.png" />')
+    // $('#jumpBox').html('<img src="assets/images/unusedjump.png" />')
+    // $('#doubleBox').html('<img src="assets/images/unusedDouble.png" />')
   }
 
   displayNextQuestion = function() {
+    $('#scoreBox').text(score);
     q = qArray.shift();
     $('#questionBox').text(q.question);
     $('#answerABox').text(q.answer1);
@@ -63,6 +72,33 @@ game = function() {
 
   }
 
+  doubleDip = function() {
+    usedDouble = true;
+    x2 = true;
+  }
+
+  fiftyFifty = function() {
+    console.log("5050");
+    used5050 = true;
+    let arr = ['#answerABox', '#answerBBox', '#answerCBox', '#answerDBox'];
+    for (i = 0; i < arr.length; i++) {
+      if ($(arr[i]).text() === q.correctAnswer()) {
+        arr.splice(i, 1);
+        break;
+      }
+    }
+    let elt = Math.floor(Math.random() * arr.length);
+    arr.splice(elt, 1);
+    for (i = 0; i < arr.length; i++) {
+      $(arr[i]).text("");
+    }
+  }
+
+  jump = function() {
+    usedJump = true;
+    displayNextQuestion();
+  }
+
   setupGame();
 
   $('#startButton').on("click", displayNextQuestion);
@@ -70,6 +106,10 @@ game = function() {
   $('.answerBox').on("click", function() {
     evaluateAnswer($(this).text());
   });
+
+  $('#5050Box').on("click", fiftyFifty);
+  $('#jumpBox').on("click", jump);
+  $('#doubleBox').on("click", doubleDip);
 
 }
 
