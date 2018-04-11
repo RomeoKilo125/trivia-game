8201cca05780deaa6311e9639c666311b330a0dc
RomeoKilo125 <romeokilo@icloud.com>
2018-04-10 22:50:50 -0400

    can now show first question

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,11 +7,11 @@ game = function() {
     qArray = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14];
     minScore = 0;
     timer = 30;
-    $('#questionBox').html('<img src="../images/startButton.png" />');
-    $('.answerBox').text("");
-    $('#5050Box').html('<img src="../images/unused5050.png" />')
-    $('#jumpBox').html('<img src="../images/unusedjump.png" />')
-    $('#doubleBox').html('<img src="../images/unusedDouble.png" />')
+    $('#questionBox').html('<img id="startButton" src="assets/images/startButton.png" />');
+    $('.answerBox').text("answers go here");
+    $('#5050Box').html('<img src="assets/images/unused5050.png" />')
+    $('#jumpBox').html('<img src="assets/images/unusedjump.png" />')
+    $('#doubleBox').html('<img src="assets/images/unusedDouble.png" />')
   }
 
   displayNextQuestion = function() {
@@ -21,10 +21,15 @@ game = function() {
     $('#answerBBox').text(q.answer2);
     $('#answerCBox').text(q.answer3);
     $('#answerDBox').text(q.answer4);
+    // setInterval(1000)
 
   }
 
+
   setupGame();
+
+  $('#startButton').on("click", displayNextQuestion);
+
 }
 
 game()
